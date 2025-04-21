e478bc377565616e768df37b2cb2d23e4b2a5940
Jackson Veroneze <jacksonvze@gmail.com>
2025-04-21 19:04:52 -0300

    feat: ajustes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './pages/errors/not-found/not-found.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
         path: '',
         redirectTo: '/login',
         pathMatch: 'full'
+    },
+    {
+        path: '**',
+        component: NotFoundComponent
     },
 ];
